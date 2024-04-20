0683f498f7c103a7ee4dedb38673c3293c71f7f8
davexxx <davexxx@163.com>
2024-04-20 15:35:55 +0800

    fix bug

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -51,7 +51,7 @@ class ChatGPTBot(Bot, OpenAIImage):
                 openai.api_base = conf().get("open_ai_api_base")
 
             if "llama3" in conf().get("model"):
-                query = f"{query}{",请用中文回复"}"
+                query = f"{query}{',请用中文回复'}"
                 logger.info("[CHATGPT] actual query={}".format(query))
 
             session_id = context["session_id"]
