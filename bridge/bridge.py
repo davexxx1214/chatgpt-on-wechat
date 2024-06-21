6761846a4d32fed704cf229ef7d1bd47aa9ad3f3
davexxx <davexxx@163.com>
2024-06-21 09:25:40 +0800

    fix bug on claude3.5

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -36,7 +36,7 @@ class Bridge(object):
             self.btype["chat"] = const.GEMINI
         if model_type in [const.ZHIPU_AI]:
             self.btype["chat"] = const.ZHIPU_AI
-        if model_type and model_type.startswith("claude-3"):
+        if model_type and model_type.startswith("claude-3") and not model_type.startswith("claude-3-5"):
             self.btype["chat"] = const.CLAUDEAPI
 
         if model_type in ["claude"]:
