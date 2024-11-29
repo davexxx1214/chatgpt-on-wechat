4d40f6d6b77f3b6aa3cc99c9c17d1835f22b9ba1
davexxx <davexxx@163.com>
2024-11-29 18:27:59 +0800

    为语音合成增加音色。

--- plugins/sovits/sovits.py
+++ plugins/sovits/sovits.py
@@ -107,15 +107,29 @@ class sovits(Plugin):
 
             else:
                 if content.startswith(self.azure_tts_prefix):
-                    pattern = self.azure_tts_prefix + r"\s(.+)"
+                    pattern = self.azure_tts_prefix + r"\s*([女男][12])?\s*(.+)?"
                     match = re.match(pattern, content)
-                    tip = f"💡欢迎使用语音合成服务(可商用)，语音合成指令格式为:\n\n{self.azure_tts_prefix}+空格+文字"
+                    voice_mappings = {
+                        "女1": "zh-CN-XiaochenMultilingualNeural",
+                        "女2": "zh-CN-XiaoyuMultilingualNeural",
+                        "男1": "zh-CN-YunfanMultilingualNeural",
+                        "男2": "zh-CN-YunyiMultilingualNeural"
+                    }
+                    tip = f"💡欢迎使用语音合成服务(可商用)，语音合成指令格式为:\n\n{self.azure_tts_prefix} [音色] 文字\n\n可选音色：女1、女2、男1、男2\n例如：语音合成 女1 你好\n不指定音色则使用默认音色"
+                    
                     if match:
-                        tts_text = content[len(self.azure_tts_prefix):].strip()
-                        azure_voice_service = AzureVoice()
-                        reply = azure_voice_service.textToVoice(tts_text)
+                        voice_type = match.group(1)
+                        text = match.group(2)
+                        
+                        if text:
+                            azure_voice_service = AzureVoice()
+                            if voice_type:
+                                azure_voice_service.speech_config.speech_synthesis_voice_name = voice_mappings[voice_type]
+                            reply = azure_voice_service.textToVoice(text.strip())
+                        else:
+                            reply = Reply(type=ReplyType.TEXT, content=tip)
                     else:
-                        reply = Reply(type=ReplyType.TEXT, content= tip)
+                        reply = Reply(type=ReplyType.TEXT, content=tip)
 
                     e_context["reply"] = reply
                     e_context.action = EventAction.BREAK_PASS
